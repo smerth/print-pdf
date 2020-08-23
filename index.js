a2b8cb21ae6a734500f40b1e2fbfb99d6a1371d8
Smerth <stephen.merth@gmail.com>
2020-08-23 13:48:37 -0700

    added beginning of CLI interface to index.js

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-const puppeteer = require("puppeteer");
+const commander = require("commander");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const clear = require("clear");
 
-(async () => {
-    //   const browser = await puppeteer.launch()
-    const browser = await puppeteer.launch({
-        headless: true,
+const files = require("./lib/files");
+
+commander
+    .command("init")
+    .description("Draw app banner")
+    .action(() => {
+        clear();
+        console.log(
+            chalk.magenta(
+                figlet.textSync("printPDF", { horizontalLayout: full })
+            )
+        );
     });
-    //   const page = await browser.newPage()
-    const page = await browser.newPage();
-    //   await page.goto("http://localhost:8000/web/work", {
-    //     waitUntil: ["load", "networkidle0", "domcontentloaded"],
-    //   })
-    //   await page.goto("http://localhost:8000/web/work")
-    await page.goto("http://localhost:8000/web/work", {
-        waitUntil: "networkidle2",
-    });
 
-    await page.emulateMediaType("print");
-    let pdf;
-
-    const type = "single";
-
-    if (type === "single") {
-        const height = await page.evaluate(() => {
-            const { body } = document;
-            const html = document.documentElement;
-
-            const maxHeight = Math.max(
-                body.scrollHeight,
-                body.offsetHeight,
-                html.clientHeight,
-                html.scrollHeight,
-                html.offsetHeight
-            );
-
-            return maxHeight;
-        });
-        pdf = await page.pdf({
-            path: "resume.pdf",
-            printBackground: true,
-            width: `21cm`,
-            height: `${height}px`,
-            pageRanges: "1",
-        });
-    } else {
-        pdf = await page.pdf({
-            path: "resume.pdf",
-            format: "A4",
-            printBackground: true,
-        });
-    }
-
-    await browser.close();
-    return Buffer.from(pdf).toString("base64");
-})();
+commander.parse(process.argv);
+if (!commander.args.length) {
+    commander.help();
+}
