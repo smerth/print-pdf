10f8b2532a4f276e0eca44c604843286f4df0249
Smerth <stephen.merth@gmail.com>
2020-08-28 10:22:15 -0700

    working print function.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const figlet = require("figlet");
 const clear = require("clear");
 
 const printPdf = require("./lib/printPDF");
+const input = require("./lib/input");
+
+// const pkg = "./package.json";
+// const conf = new Configstore(pkg.name);
 // const files = require("./lib/files");
 
 commander
@@ -26,14 +30,34 @@ commander
                 figlet.textSync("********", { horizontalLayout: "full" })
             )
         );
+        const answers = async () => {
+            const answers = await input.askPrintOptions();
+            return answers;
+        };
+        console.log("Answers: ", answers);
     });
+// .description("Fetch user input from command line")
+// .action(async () => {
+//     const answers = await input.askPrintOptions();
+//     console.log("Answers: ", answers);
+// });
+
+commander
+    .command("print")
+    .description("print a PDF from user input from the command line")
+    .action(async () => {
+        const answers = await input.askPrintOptions();
+        printPdf.print(answers);
+    });
+
+// const answers = await input.getUserInput;
+
+// TODO: get input from user
+// TODO: pass input to print function
+
+// printPdf.print();
 
 commander.parse(process.argv);
 if (!commander.args.length) {
     commander.help();
 }
-
-// TODO: get input from user
-// TODO: pass input to print function
-
-printPdf.print();
